f4b29140937d0f8f8254f1e80b88e61d1f518670
ShashankGanapatiNaik <shashanknaik6226@gmail.com>
2026-09-22 11:35:53 +0530

    Made a navbar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,21 @@
+function SearchBar({ value, onChange, onSearch }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch();
+  };
+
+  return (
+    <form className="search-bar" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search medicine by brand name"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default SearchBar;
